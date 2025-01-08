80ae467f949d168ba067d7955b81da0fd48b8d3d
Yotam Greenstein <yotamgr314@gmail.com>
2025-01-08 21:55:15 +0200

    complete IPC and fork explaination
    complete IPC and fork explaination

--- compiledFilesToLoad/A.c
+++ compiledFilesToLoad/A.c
@@ -5,20 +5,22 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <read_fd> <write_fd>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <read_fd> <write_fd>\n", argv[0]); // printing error message in the standart error channel. <read_fd> and <write_fd> are just placeholders to the expected arguments and argv[0] is the name of the programmn (as we passed via excv in loader.c)
         exit(EXIT_FAILURE);
     }
 
-    // Convert file descriptors from strings to integers
+    // NOTE: Convert file descriptors from strings to integers (becuse when we passed the file descriptors to the const char* argv[] we are to convert them to their corresponding char* values).
     int read_fd = atoi(argv[1]);
     int write_fd = atoi(argv[2]);
 
-    // Close the read end since this process will only write
-    close(read_fd);
+    
+    //NOTE: int close(int fd); --> Closes an open file descriptor.
+    close(read_fd); // Close the read end since this process will only write
 
-    // Write a message to the pipe
     const char *message = "Hello from A!";
-    if (write(write_fd, message, strlen(message)) == -1) {
+    //NOTE: size_t write(int fileDescriptor, const void *buf, size_t count); --> where int filedescriptor file descriptor to which you want to write data. void* buf is a pointer to the buffer containing the data to write. and size_t count is The number of bytes to write from the buffer.
+    if (write(write_fd, message, strlen(message)) == -1) // write a message ot the pipe.
+    {
         perror("write");
         exit(EXIT_FAILURE);
     }
--- compiledFilesToLoad/B.c
+++ compiledFilesToLoad/B.c
@@ -4,19 +4,21 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <read_fd> <write_fd>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <read_fd> <write_fd>\n", argv[0]); // printing error message in the standart error channel. <read_fd> and <write_fd> are just placeholders to the expected arguments and argv[0] is the name of the programmn (as we passed via excv in loader
         exit(EXIT_FAILURE);
     }
 
-    // Convert file descriptors from strings to integers
+    // NOTE: Convert file descriptors from strings to integers (becuse when we passed the file descriptors to the const char* argv[] we are to convert them to their corresponding char* values).
     int read_fd = atoi(argv[1]);
     int write_fd = atoi(argv[2]);
 
-    // Close the write end since this process will only read
-    close(write_fd);
+     //NOTE: int close(int fd); --> Closes an open file descriptor.
+    close(write_fd);// Close the write end since this process will only read
 
-    // Read a message from the pipe
-    char buffer[128];
+    
+    char buffer[128]; // Read a message from the pipe
+
+    //NOTE: ssize_t read(int fd, void *buf, size_t count); --> int fd is the file desctriptor to read from (pipe/file/socket). void * buf is a pointer to the buffer where the read data will be stored. size_t count is the maximu, mnumber of bytes to be read into the buffer.
     ssize_t bytes_read = read(read_fd, buffer, sizeof(buffer) - 1);
     if (bytes_read > 0) {
         buffer[bytes_read] = '\0';  // Null-terminate the string
