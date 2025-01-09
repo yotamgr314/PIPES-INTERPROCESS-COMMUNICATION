12c2aa763307684d7259992b8eab9121dc7ba858
Yotam Greenstein <yotamgr314@gmail.com>
2025-01-09 18:33:39 +0200

    moved to double pipes
    moved to double pipes

--- compiledFilesToLoad/A.c
+++ compiledFilesToLoad/A.c
@@ -5,30 +5,23 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <read_fd> <write_fd>\n", argv[0]); // printing error message in the standart error channel. <read_fd> and <write_fd> are just placeholders to the expected arguments and argv[0] is the name of the programmn (as we passed via excv in loader.c)
+        fprintf(stderr, "Usage: %s <write_fd> <read_fd>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // NOTE: Convert file descriptors from strings to integers (becuse when we passed the file descriptors to the const char* argv[] we are to convert them to their corresponding char* values).
-    int read_fd = atoi(argv[1]);
-    int write_fd = atoi(argv[2]);
+    // Convert the file descriptor strings to integers
+    int write_fd = atoi(argv[1]);
+    int read_fd = atoi(argv[2]);
 
-    
-    //NOTE: int close(int fd); --> Closes an open file descriptor.
-    close(read_fd); // Close the read end since this process will only write
+    // Close the unused read end
+    close(read_fd);
 
-    const char *message = "Hello from A!";
-    //NOTE: size_t write(int fileDescriptor, const void *buf, size_t count); --> where int filedescriptor file descriptor to which you want to write data. void* buf is a pointer to the buffer containing the data to write. and size_t count is The number of bytes to write from the buffer.
-    if (write(write_fd, message, strlen(message)) == -1) // write a message ot the pipe.
-    {
-        perror("write");
-        exit(EXIT_FAILURE);
-    }
+    // Send a message to Child 2
+    const char *message_to_B = "Hello from A!";
+    write(write_fd, message_to_B, strlen(message_to_B));
+    printf("A: Sent message to B: %s\n", message_to_B);
 
-    printf("A: Sent message to the pipe.\n");
-    fflush(stdout);
-
-    // Close the write end
+    // Close the write end after sending the message
     close(write_fd);
 
     return 0;
--- compiledFilesToLoad/B.c
+++ compiledFilesToLoad/B.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <read_fd> <write_fd>\n", argv[0]); // printing error message in the standart error channel. <read_fd> and <write_fd> are just placeholders to the expected arguments and argv[0] is the name of the programmn (as we passed via excv in loader
+        fprintf(stderr, "Usage: %s <write_fd> <read_fd>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // NOTE: Convert file descriptors from strings to integers (becuse when we passed the file descriptors to the const char* argv[] we are to convert them to their corresponding char* values).
-    int read_fd = atoi(argv[1]);
-    int write_fd = atoi(argv[2]);
+    // Convert the file descriptor strings to integers
+    int write_fd = atoi(argv[1]);
+    int read_fd = atoi(argv[2]);
 
-     //NOTE: int close(int fd); --> Closes an open file descriptor.
-    close(write_fd);// Close the write end since this process will only read
-
-    
-    char buffer[128]; // Read a message from the pipe
-
-    //NOTE: ssize_t read(int fd, void *buf, size_t count); --> int fd is the file desctriptor to read from (pipe/file/socket). void * buf is a pointer to the buffer where the read data will be stored. size_t count is the maximu, mnumber of bytes to be read into the buffer.
+    // Read a message from Child 1
+    char buffer[256];
     ssize_t bytes_read = read(read_fd, buffer, sizeof(buffer) - 1);
     if (bytes_read > 0) {
         buffer[bytes_read] = '\0';  // Null-terminate the string
-        printf("B: Received message: %s\n", buffer);
-    } else {
-        perror("read");
+        printf("B: Received message from A: %s\n", buffer);
     }
 
-    // Close the read end
+    // Send a reply to Child 1
+    const char *reply_to_A = "Thank you from B!";
+    write(write_fd, reply_to_A, strlen(reply_to_A));
+    printf("B: Sent reply to A: %s\n", reply_to_A);
+
+    // Close the file descriptors
     close(read_fd);
+    close(write_fd);
 
     return 0;
 }
