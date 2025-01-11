5266bb4159b4652f5737eaf29e7493875eef11f7
Yotam Greenstein <yotamgr314@gmail.com>
2025-01-11 20:02:10 +0200

    שדגשד
    שדגשדג

--- compiledFilesToLoad/A.c
+++ compiledFilesToLoad/A.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -22,21 +23,29 @@ int main(int argc, char *argv[]) {
     // child1 reads from file1 and send chunks to child2.
     char buffer[256];
     ssize_t bytes;
-    while ((bytes = read(file, buffer, sizeof(buffer))) > 0) {
+    printf("\nchild1: sends the content he read from file1 to child2.\n");
+    while ((bytes = read(file, buffer, sizeof(buffer))) > 0)
+    {
         write(write_fd, buffer, bytes);
     }
+
     close(file);
 
     // child1 Read response message from childs2 via the pipe.
     char response[256];
     read(read_fd, response, sizeof(response));
-    printf("Child1 received response: %s\n", response);
+    printf("Child1: recivied response from child2 - %s\n", response);
+
+    // Print final message before exit
+    printf("Child1: My life has reached its worthy end. Goodbye.\n");
+
+    //Send SIGUSR1 signal to loader (the parent process) before exiting so it can inc the revival counter.
+    kill(getppid(), SIGUSR1);
+    printf("Child1: Sent SIGUSR1 to loader before exiting.\n\n");
 
     // Close the file descriptors
     close(write_fd);
     close(read_fd);
 
-    // Print final message before exit
-    printf("Child1: My life has reached its worthy end. Goodbye.\n");
     exit(0);
 }
--- compiledFilesToLoad/B.c
+++ compiledFilesToLoad/B.c
@@ -5,7 +5,9 @@
 #include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+
+    if (argc != 3)
+    {
         fprintf(stderr, "Usage: %s <write_fd> <read_fd>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -14,31 +16,32 @@ int main(int argc, char *argv[]) {
     int read_fd = atoi(argv[2]);
 
     // Open fileB.txt for writing
-    int file = open("fileB.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
-    if (file == -1) {
+    int file2 = open("fileB.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (file2 == -1) {
         perror("open fileB.txt");
         exit(EXIT_FAILURE);
     }
 
-    // Read a message from Child 1
+    // Child2 Read a message from Child 1
     char buffer[256];
     ssize_t bytes_read = read(read_fd, buffer, sizeof(buffer) - 1);
-    if (bytes_read > 0) {
+    if (bytes_read > 0) // if reaciving the message from child1 was sucesfull.
+    {
         buffer[bytes_read] = '\0';  // Null-terminate the string
-        printf("child2: Received message from child1: %s\n", buffer);
+        printf("\nchild2 Received message from child1 -  %s\n", buffer);
 
         // Write the message to fileB.txt
-        write(file, buffer, bytes_read);
-        write(file, "\n", 1);
+        write(file2, buffer, bytes_read);
+        write(file2, "\n", 1);
     }
 
     // Send a reply to Child 1
     const char *reply_to_child1 = "Thank you for the informainon, ive recivied it.";
     write(write_fd, reply_to_child1, strlen(reply_to_child1));
-    printf("child2: Sent reply to child1: %s\n", reply_to_child1);
+    printf("child2: Sends message to child1 -  %s\n", reply_to_child1);
 
     // Close the file descriptors
-    close(file);
+    close(file2);
     close(read_fd);
     close(write_fd);
 
